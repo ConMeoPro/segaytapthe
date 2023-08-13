d04aae3cffbc102c11a0b11f203077fad95af5fd
ConMeoPro <100891502+ConMeoPro@users.noreply.github.com>
2023-08-13 20:58:02 +0700

    Add files via upload

---
+++ Maru Hub.lua
@@ -0,0 +1,3 @@
+(getgenv()).key = "MARUU8HP0Z2FA1SYZSIA0M7JWL";
+(getgenv()).id = "912996856722755629";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
