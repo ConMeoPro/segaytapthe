fa27ec2bc1eb1673754c271912123cf05019f840
ConMeoPro <100891502+ConMeoPro@users.noreply.github.com>
2023-08-14 19:33:05 +0700

    Update and rename Maru Hub.lua to Alchemy.lua

--- Maru Hub.lua
+++
@@ -1,3 +0,0 @@
-(getgenv()).key = "MARUNQ1N2PG3YP7MB5LVV92SEK";
-(getgenv()).id = "948383315587653652";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
